a0c95b4bf65feca6a303e79b70ba3e6d0c24251b
dokechin <ttatsumi@ra2.so-net.ne.jp>
2019-04-03 21:06:38 +0900

    fix doc

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   SERVER_PORT: process.env.SERVER_PORT || 3000,
-  DOMAIN: 'https://dokechin-pushup.glitch.me/clova',
-  ExtensionId: 'com.dokechin.pushup',
+  DOMAIN: 'https://dokechin-pushup.dokechin.com/clova',
+  ExtensionId: 'com.dokechin.kintorecheer',
   DEBUG: process.env.DEBUG || false,
 }
