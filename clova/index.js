e4cec9209539c6b8698e74b4ab169342ba086e33
dokechin <ttatsumi@ra2.so-net.ne.jp>
2019-04-06 19:56:57 +0900

    add debug code

--- clova/index.js
+++ clova/index.js
@@ -32,6 +32,7 @@ class CEKRequest {
         return this.launchRequest(cekResponse)
       case 'IntentRequest':
         await this.intentRequest(cekResponse);
+        console.log("intentRequest end")
         return cekResponse;
       case 'SessionEndedRequest':
         return this.sessionEndedRequest(cekResponse)
