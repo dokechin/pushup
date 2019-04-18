f5e6c53a1cb37177686f813ab2eae47e39027b2c
Tetsuya Tatsumi <ttatsumi@ra2.so-net.ne.jp>
2019-04-18 21:02:44 +0900

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  SERVER_PORT: process.env.SERVER_PORT || 3000,
-  DOMAIN: 'https://dokechin-pushup.dokechin.com/clova',
-  ExtensionId: 'com.dokechin.kintorecheer',
-  DEBUG: process.env.DEBUG || false,
-  BOT_ACCESS_TOKEN: 'XKOParcfWHennQkUmg965OK5PBtEK1hRC2xZaO7ZeKSgHr9xTK6rXoqzfMNN5bkX9jb5rCphxf8sS98OY24xQXZ4ulJkSa5DRlzq1afjn+76OxlNdfMSVl++mbjbKnPh8k22WZfoQthF+1ltL4nQcwdB04t89/1O/w1cDnyilFU=',
-}
