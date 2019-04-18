1eac75a8a3095cc7163ae838f7bab6df16911a89
dokechin <ttatsumi@ra2.so-net.ne.jp>
2019-04-18 21:01:06 +0900

    fix config

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ module.exports = {
   DOMAIN: 'https://dokechin-pushup.dokechin.com/clova',
   ExtensionId: 'com.dokechin.kintorecheer',
   DEBUG: process.env.DEBUG || false,
-  BOT_ACCESS_TOKEN: 'bf4e0c10b59254c9aa99e9bca3ddf7e3',
+  BOT_ACCESS_TOKEN: 'XKOParcfWHennQkUmg965OK5PBtEK1hRC2xZaO7ZeKSgHr9xTK6rXoqzfMNN5bkX9jb5rCphxf8sS98OY24xQXZ4ulJkSa5DRlzq1afjn+76OxlNdfMSVl++mbjbKnPh8k22WZfoQthF+1ltL4nQcwdB04t89/1O/w1cDnyilFU=',
 }
