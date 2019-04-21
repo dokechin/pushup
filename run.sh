7585505a64525f9ac60411c9e4460a06d8ea66eb
dokechin <ttatsumi@ra2.so-net.ne.jp>
2019-04-21 13:00:38 +0000

    fix

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PATH=/usr/local/bin:$PATH
 cd `dirname $0`
-exec  - TZ=Asia/Tokyo;PATH=$PATH ~/.ndenv/shims/node app.js
+exec  -i TZ=Asia/Tokyo PATH=$PATH ~/.ndenv/shims/node app.js
