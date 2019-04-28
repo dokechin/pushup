4156e5b05376ebe7415a0fcf188dc392fcdde099
dokechin <ttatsumi@ra2.so-net.ne.jp>
2019-04-28 19:59:05 +0900

    add t

--- batch/index.js
+++ batch/index.js
@@ -7,6 +7,7 @@ const client = new line.Client({
 var { Client } = require('pg');
 var {MENU_TYPE} = require('../model/menu.js');
 var moment = require('moment-timezone');
+var math = require('math');
 
 
 class Batch {
@@ -51,6 +52,25 @@ class Batch {
       if (res && res.rows.length > 0){
         rank[menu] = res.rows;
       }
+
+      const query2 = {
+        text: `select menu, round(avg(count)) as avg, roung(stddev(count)) as std
+            from (
+                select
+                    menu.menu,menu.menu_id,user_id,
+                    sum(count) as count
+                from train
+                inner join menu
+                on train.menu_id = menu.menu_id
+                where execute_date between $1 and $2 and train.menu_id = $3
+                group by 1
+            ) s`,
+        values: [firstDay,lastDay],
+      }
+      var res2 = await this.pgclient.query(query2);
+      if (res2 && res2.rows.length > 0){
+        sta[menu] = {avg : res2.rows[0].avg, std : res2.rows[0].std};
+      }
     }
 
     const query2 = {
@@ -73,20 +93,31 @@ class Batch {
     var bMonth = new moment().subtract(1,'months')
     var text = bMonth.year() + "年" + (bMonth.month() + 1 ) + "月の成績発表 \uDBC0\uDCB4 \n";
     for(var i=0;i<res2.rows.length;i++){
-      text = text + res2.rows[i].menu + " : " +res2.rows[i].count + "回";
+
+      var avg = sta[res2.rows[i].menu].avg;
+      var std = sta[res2.rows[i].menu].std;
+      var t = math.round((10 * (res2.rows[i].count - avg) / std) + 50);
+
+      text = text + res2.rows[i].menu + " 参加者平均" + avg + "回\n";
+
+      text = text + "あなた " + res2.rows[i].count + "回 ";
+      text = text + "偏差値 " + t;
+
       for (var j=0;j<rank[res2.rows[i].menu].length;j++){
         if (res2.rows[i].user_id == rank[res2.rows[i].menu][j].user_id){
-          text = text + "(" + rank[res2.rows[i].menu][j].rn+ "位 入賞 \uDBC0\uDC73 )";
+          text = text + " (" + rank[res2.rows[i].menu][j].rn+ "位 入賞 \uDBC0\uDC73 )";
         }
       }
+
       text = text + "\n";
+
       if (i == (res2.rows.length-1) || res2.rows[i].user_id != res2.rows[i+1].user_id){
         console.log(text)
-        client.pushMessage(res2.rows[i].user_id, {
+/*        client.pushMessage(res2.rows[i].user_id, {
           type: 'text',
           text: text
-        });
-        text = bMonth.year() + "年" + (bMonth.month() + 1 ) + "月の集計結果\n";
+        });*/
+        text = bMonth.year() + "年" + (bMonth.month() + 1 ) + "月の成績発表\n";
       }
     }
     this.pgclient.end()
